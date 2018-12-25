31f9f92be1b637654ca6a30cf80fdbbbb6e5ac11
Casey Davis <cbdavis33@gmail.com>
2018-12-24 21:10:07 -0800

    Added documentation

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(...) {
-  bcgpLib <- dirname(system.file(package = "bcgp1"))
-  pkgdesc <- packageDescription("bcgp1", lib.loc = bcgpLib)
+  bcgpLib <- dirname(system.file(package = "bcgp0a"))
+  pkgdesc <- packageDescription("bcgp0a", lib.loc = bcgpLib)
   packageStartupMessage(paste("bcgp (Version ", pkgdesc$Version,")", sep = ""))
   packageStartupMessage("For execution on a local, multicore CPU with excess RAM call\n",
                         "options(mc.cores = parallel::detectCores()).")
